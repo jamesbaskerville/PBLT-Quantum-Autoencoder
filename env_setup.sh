33788a8d694aafc76cee4679d6973cf212e4f8b1
leepeter6564 <leepeter6564@hotmail.com>
2018-04-05 15:06:25 -0400

    edited environment setup

--- env_setup.sh
+++ env_setup.sh
@@ -3,5 +3,7 @@ mkdir -p $WORKON_HOME
 source setup/virtualenvwrapper.sh
 mkvirtualenv pblt
 pip3 install numpy
+pip3 install Cython
+pip3 install scipy
 pip3 install -r setup/requirements.txt
 lssitepackages
