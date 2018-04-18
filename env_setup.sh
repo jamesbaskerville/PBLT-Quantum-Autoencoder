82f8e7ea38f2ef73b118f4c05894b74891a11e57
leepeter6564 <leepeter6564@hotmail.com>
2018-04-18 15:09:09 -0400

    included installation of openfermion + psi4

--- env_setup.sh
+++ env_setup.sh
@@ -6,5 +6,7 @@ pip3 install numpy
 pip3 install Cython
 pip3 install scipy
 pip3 install -r setup/requirements.txt
+pip3 install openfermion
+pip3 install openfermionpsi4
 ipython kernel install --user --name=pblt
 lssitepackages
