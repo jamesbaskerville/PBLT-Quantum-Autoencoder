de693706e7cd6188080fa0c8a640c8068ef54f24
James Baskerville <jameswbaskerville@gmail.com>
2018-04-18 17:38:59 -0400

    reproduced figure 6

--- OpenFermionTest/OpenFermionTest.py
+++ OpenFermionTest/OpenFermionTest.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[8]:
+# In[43]:
 
 
 # https://github.com/quantumlib/OpenFermion-Psi4/blob/master/examples/openfermionpsi4_demo.ipynb
@@ -9,17 +9,18 @@
 from openfermion.hamiltonians import MolecularData
 from openfermionpsi4 import run_psi4
 import matplotlib.pyplot as plt
-from openfermion.transforms import get_fermion_operator, jordan_wigner
+from openfermion.transforms import get_fermion_operator, jordan_wigner, get_sparse_operator
+from qutip import *
 
 
-# In[55]:
+# In[68]:
 
 
 # Set molecule parameters.
 basis = 'sto-3g'
 multiplicity = 1
-bond_length_interval = 0.2
-n_points = 10
+bond_length_interval = 0.05
+n_points = 55
 
 # Set calculation parameters.
 run_scf = 1
@@ -60,51 +61,71 @@ for point in range(1, n_points + 1):
     hamiltonians += [hamiltonian]
 
 
-# In[61]:
+# In[51]:
 
 
-from qutip import *
-letter_to_op = {
-    'X':sigmax(),
-    'Y':sigmay(),
-    'Z':sigmaz()
-}
-def makepaulin(info):
-    n, l = info
-    op = letter_to_op(l)
-    tmp = [qeye(2) for _ in range(n)]
-    tmp[n] = op
-    return tensor(tmp)
-        
+def gen_eigenstates(qf_hamiltonians):
+    groundenergies = []
+    groundstates = []
+    for qf_ham in qf_hamiltonians:
+        ham = Qobj(get_sparse_operator(qf_ham).todense())
+        eigenstates = ham.eigenstates()
+        i = np.argmin(eigenstates[0])
+        groundenergy, groundstate = eigenstates[0][i], eigenstates[1][i]
+        groundenergies += [groundenergy]
+        groundstates += [groundstates]
+    return groundenergies, groundstates
 
 
-# In[60]:
+# In[69]:
 
 
-import numpy as np
-terms = hamiltonians[0].terms
-keys = list(terms.keys())
-keys
+groundenergies, groundstates = gen_eigenstates(hamiltonians)
 
 
-# In[59]:
+# In[72]:
 
 
-# create tensored identity in hilbert space of n qubits
-def tenseye(n):
-    return tensor([qeye(2) for _ in range(n)])
+plt.plot(bond_lengths, groundenergies, 'bo-')
+plt.ylim([-1.15, -0.90])
+plt.xlim([0.3, 3.0])
 
 
-# In[54]:
+# In[55]:
 
 
-def ham_to_op(n, of_hamiltonian):
-    terms = of_hamiltonian.terms
+# # Self-implementation
+# letter_to_op = {
+#     'X':sigmax(),
+#     'Y':sigmay(),
+#     'Z':sigmaz()
+# }
+
+# def makepaulin(n, info):
+#     tgt, l = info
+#     op = letter_to_op[l]
+#     tmp = [qeye(2) for _ in range(n)]
+#     tmp[tgt] = op
+#     return tensor(tmp)
+
+# def multipaulin(n, lstinfo):
+#     res = qeye([2] * n)
+#     for info in lstinfo:
+#         res *= makepaulin(n, info)
+#     return res
+
+# def ham_to_op(n, of_hamiltonian):
+#     terms = of_hamiltonian.terms
     
-    # generate zeroed operator of correct dimensions
-    op = tenseye(n) - tenseye(n)
+#     # generate zeroed operator of correct dimensions
+#     op = tenseye(n) - tenseye(n)
     
-    # iterate through terms of hamiltonian
-    for key in list(terms.keys()):
-        
+#     # iterate through terms of hamiltonian
+#     for key in list(terms.keys()):
+#         if not key:
+#             op += qeye([2] * n) * terms[key]
+#         else:
+#             # coefficient * product of operators
+#             op += terms[key] * multipaulin(n, key)
+#     return op
 
